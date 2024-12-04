c2ecc55955229e06067361e43be18e45413704bd
denisfitz57 <denis.fitzpatrick@boystown.org>
2024-12-04 11:05:26 -0600

    Removed InstallPathTmp.m, updated Readme to reflect that

--- STLAnalysis_JASA/InstallPathTmp.m
+++
@@ -1,9 +0,0 @@
-% Run script to temporarily reset the MATLAB search path and add the
-% required folders for the current MATLAB session only.
-restoredefaultpath
-addpath(genpath([pwd '\altmany-export_fig-3.47.0.0']))
-addpath(genpath([pwd '\extrema']))
-addpath(genpath([pwd '\gridtrimesh']))
-addpath(genpath([pwd '\polygon2voxel']))
-addpath(genpath([pwd '\utilities']))
-clear all
